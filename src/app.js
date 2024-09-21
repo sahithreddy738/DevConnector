dae42513850a87d8b4f6977f1f6db849b041ff35
Sahith Reddy <sahithreddy738@gmail.com>
2024-09-21 22:02:57 +0530

    created routes

--- src/app.js
+++ src/app.js
@@ -2,8 +2,27 @@ const express=require("express");
 
 const app=express();
 
-app.use("/",(req,res)=>{
-   res.send("HomePage");
+// app.use("/",(req,res)=>{
+//     res.send("hello")
+// })
+app.use("/hello",(req,res)=>{
+    res.send("Hello from hello route");
+})
+app.get("/user",(req,res)=>{
+    res.send({"id":req.query.userId,"firstName":"sahith","city":"Hyderabad"});
+})
+app.get("/user/:userId",(req,res)=>{
+    console.log(req.params);
+    res.send({"id":req.params.userId,"firstName":"sahith","city":"Hyderabad"});
+})
+app.post("/user",(req,res)=>{
+     res.send("Data Saved Successfully");
+})
+app.patch("/user",(req,res)=>{
+    res.send("Updated Data Successfully");
+})
+app.delete("/user",(req,res)=>{
+    res.send("Data deleted successfully");
 })
 
 app.listen(3000,() => {
